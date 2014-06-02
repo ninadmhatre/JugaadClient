f89413659f13a8f70a9eb10f8f9ebc197788ed6a
nathan <nathan@yearaway.com>
2014-06-02 12:35:13 +0100

    preparing for a build

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -3,7 +3,7 @@
 cd test/ServerSideStubs
 
 echo "Start the server side stubs which mimic the master server communication"
-/usr/local/bin/python3.4 test/ServerSideStubs/TaskRequesterStartAll.py
+/usr/local/bin/python3.4 TaskRequesterStartAll.py
 
 echo "Run a client test"
 
