f86686a4da3a8d0bcdfe8ea49d13691b581fb75d
nathan <nathan@yearaway.com>
2014-06-02 12:34:08 +0100

    preparing for a build

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+cd test/ServerSideStubs
+
 echo "Start the server side stubs which mimic the master server communication"
-/usr/local/bin/python3.4 "Jugaad Client"/test/ServerSideStubs/TaskRequesterStartAll.py
+/usr/local/bin/python3.4 test/ServerSideStubs/TaskRequesterStartAll.py
 
 echo "Run a client test"
 
